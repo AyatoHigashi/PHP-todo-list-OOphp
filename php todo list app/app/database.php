06fbc9ef418106c63a1586028522a0aec21a58bb
AyatoHigashi <62373542+AyatoHigashi@users.noreply.github.com>
2021-06-05 02:49:15 +0600

    Add files via upload

---
+++ php todo list app/app/database.php
@@ -0,0 +1,19 @@
+<?php
+
+class Database {
+
+	private $host = 'localhost';
+	private $username = 'root';
+	private $password = '';
+	private $dbname = 'todo_list';
+
+	protected function connect() {
+
+		$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+		$pdo = new PDO($dsn, $this->username, $this->password);
+		$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+		return $pdo;
+
+	}
+} /// end class Database
